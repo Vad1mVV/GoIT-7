9b4a366a5e71f5c3c8c8768a2df4efba5b0c7432
Vad1mVV <107924193+Vad1mVV@users.noreply.github.com>
2023-01-21 12:42:46 +0300

    2 exercise

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+enterImgMarkup();
+
+function enterImgMarkup() {
+    galleryBox.insertAdjacentHTML("beforeend", createImgMarkup(galleryItems))
+}
+function createImgMarkup(object) {
+    return object.map(({ preview, original, description }) => {
+        return  `<a class="gallery__item" href="${original}">
+                    <img 
+                        class="gallery__image" 
+                        src="${preview}" 
+                        alt="${description}" 
+                    />
+                </a>`
+    }).join('');
+}
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: "alt",
+    captionDelay: 250,
+});
